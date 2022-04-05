f31039689447a949b671f9211714e49f069bf1aa
Cheyu <Cheyujlee@gmail.com>
2022-04-05 11:35:51 -0700

    04-05 initialize samtools portion

--- module-detection.py
+++ module-detection.py
@@ -14,6 +14,7 @@
 ########################################################################################################################
 import argparse
 import os
+import pysam
 
 
 ########################################################################################################################
@@ -69,6 +70,9 @@ def main():
     print(map_human())
     os.rename(args.out + "/human/Unmapped.out.mate1", args.out + "/human/Unmapped.out.mate1.fastq")
     print(map_virus())
+    pysam.view("-h", "-o",
+               args.out + "/virus/Aligned.sortedByCoord.sam",
+               args.out + "/virus/Aligned.sortedByCoord.bam")
     return
 
 
--- scripts/module1-detection_bulk.sh
+++ scripts/module1-detection_bulk.sh
@@ -1,106 +1,91 @@
 #!/bin/bash
-#SBATCH --job-name=HIVntgr6
+#SBATCH --job-name=generateTest
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=4G
 #SBATCH --partition=zhanglab.p
-#SBATCH --output=/srv/disk00/cheyul1/logs/09-19-21/HIVntgr6.log
+#SBATCH --output=/srv/disk00/cheyul1/Venus/logs/22-04-05/generateTest.log
 
-run_prefix=HIVntgr6
-out_dir=/srv/disk00/cheyul1/outputs/09-19-21
+run_prefix=generateTest
+out_dir=/srv/disk00/cheyul1/Venus/outputs/22-04-05
 STAR_dir=/srv/disk00/cheyul1/Venus/STAR
 indices_dir=${STAR_dir}/indices
+data_dir=/srv/disk00/cheyul1/Venus/datasets/YaChi_GSE112576
+numfiles=1
 
-fastq_links=( https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-19/SRR6944369/SRR6944369.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/SRR6944370/SRR6944370.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944371/SRR6944371.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-19/SRR6944372/SRR6944372.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-19/SRR6944373/SRR6944373.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944374/SRR6944374.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/SRR6944375/SRR6944375.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944376/SRR6944376.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944377/SRR6944377.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/SRR6944378/SRR6944378.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944379/SRR6944379.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944380/SRR6944380.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-19/SRR6944381/SRR6944381.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-19/SRR6944382/SRR6944382.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/SRR6944383/SRR6944383.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/SRR6944384/SRR6944384.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944385/SRR6944385.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-21/SRR6944386/SRR6944386.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/SRR6944387/SRR6944387.1 \
-https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/SRR6944388/SRR6944388.1 )
+fastq_links=( https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-19/SRR6944369/SRR6944369.1 )
 metatable=${STAR_dir}/new_virus.species.txt
 
 for i in ${!fastq_links[@]}; do
     ######================================ Downloads SRA runs and create directories ================================######
-    cd ${out_dir}/
-    mkdir "${run_prefix}_run$i"
-    cd "${run_prefix}_run$i"
-    mkdir "human"
-#    mkdir "virus"
-#    mkdir "mega_virus"
-    mkdir "hybrid"
-    mkdir "hybrid_sam"
-    cd human
-    mkdir "fastqs"
-    mkdir "output"
-#    cd ../virus
+#    cd ${out_dir}/
+#    mkdir "${run_prefix}_run$i"
+#    cd "${run_prefix}_run$i"
+#    mkdir "human"
+##    mkdir "virus"
+##    mkdir "mega_virus"
+#    mkdir "hybrid"
+#    mkdir "hybrid_sam"
+#    cd human
+#    mkdir "fastqs"
 #    mkdir "output"
-#    cd ../mega_virus
+##    cd ../virus
+##    mkdir "output"
+##    cd ../mega_virus
+##    mkdir "output"
+#    cd ../hybrid
 #    mkdir "output"
-    cd ../hybrid
-    mkdir "output"
-    cd ../hybrid_sam
-    mkdir "output"
+#    cd ../hybrid_sam
+#    mkdir "output"
 
     run_dir="${out_dir}/${run_prefix}_run$i"
     human_dir=${run_dir}/human
-#    virus_dir=${run_dir}/virus
-#    mega_virus_dir=${run_dir}/mega_virus
-    hybrid_dir=${run_dir}/hybrid
-    hybrid_sam_dir=${run_dir}/hybrid_sam
+    virus_dir=${run_dir}/virus
+    mega_virus_dir=${run_dir}/mega_virus
+#    hybrid_dir=${run_dir}/hybrid
+#    hybrid_sam_dir=${run_dir}/hybrid_sam
 
-    fastqs=${human_dir}/fastqs
-    cd ${fastqs}/
-    wget -nv ${fastq_links[$i]}
-    parallel-fastq-dump -s ${human_dir}/fastqs/* --threads 64 --split-files --tmpdir ${human_dir}/fastqs/tmp/ --outdir ${human_dir}/fastqs/
+#    fastqs=${human_dir}/fastqs
+#    cd ${fastqs}/
+#    wget -nv ${fastq_links[$i]}
+#    parallel-fastq-dump -s ${human_dir}/fastqs/* --threads 32 --split-files --tmpdir ${human_dir}/fastqs/tmp/ --outdir ${human_dir}/fastqs/
 #    for file in *; do
 #        fastq-dump $file --split-files --outdir .;
 #    done
 
-    for file in *; do
-        if [[ $file != *.fastq ]]; then
-            rm -rf $file
-        fi
-    done
+#    for file in *; do
+#        if [[ $file != *.fastq ]]; then
+#            rm -rf $file
+#        fi
+#    done
 
-    cd ${fastqs}/
-    numfiles=$(ls | wc -l)
-    declare -a filenames
-    for file in *.fastq; do
-        filenames=(${filenames[@]} "$file")
-    done
+#    cd ${fastqs}/
+#    numfiles=$(ls | wc -l)
+#    declare -a filenames
+#    for file in *.fastq; do
+#        filenames=(${filenames[@]} "$file")
+#    done
 
 
     ############################################################ VIRUS DETECTION ############################################################
 
     ######================================ Maps to Human index ================================######
     fastqs=${human_dir}/fastqs
-    cd ${fastqs}/
+#    cd ${fastqs}/
 
     if [[ $numfiles == 1 ]]; then
         STAR \
-        --runThreadN 64 \
+        --runThreadN 32 \
+        --readFilesCommand zcat \
         --outFileNamePrefix ${human_dir}/output/ \
         --genomeDir ${indices_dir}/human.genomeDir/ \
-        --readFilesIn ${fastqs}/${filenames[0]} \
+        --readFilesIn ${data_dir}/SRR6944349.1_1.fastq.gz \
         --outReadsUnmapped Fastx \
         --outSAMtype None
     else
         STAR \
-        --runThreadN 64 \
+        --runThreadN 32 \
+        --readFilesCommand zcat \
         --outFileNamePrefix ${human_dir}/output/ \
         --genomeDir ${indices_dir}/human.genomeDir/ \
         --readFilesIn ${fastqs}/${filenames[0]} ${fastqs}/${filenames[1]} \
@@ -111,14 +96,14 @@ for i in ${!fastq_links[@]}; do
 
     #######================================ Maps to a Target Virus index ================================######
     fastqs=${human_dir}/output
-    cd ${fastqs}/
-    mv Unmapped.out.mate1 Unmapped.out.mate1.fastq
+#    cd ${fastqs}/
+    mv ${fastqs}/Unmapped.out.mate1 ${fastqs}/Unmapped.out.mate1.fastq
 
     if test -f "Unmapped.out.mate2"; then
         mv Unmapped.out.mate2 Unmapped.out.mate2.fastq
 
         STAR \
-        --runThreadN 64 \
+        --runThreadN 32 \
         --outFileNamePrefix ${virus_dir}/output/ \
         --genomeDir ${indices_dir}/HIV.genomeDir/ \
         --readFilesIn ${fastqs}/Unmapped.out.mate1.fastq ${fastqs}/Unmapped.out.mate2.fastq \
@@ -126,7 +111,7 @@ for i in ${!fastq_links[@]}; do
         --outSAMtype BAM SortedByCoordinate
     else
         STAR \
-        --runThreadN 64 \
+        --runThreadN 32 \
         --outFileNamePrefix ${virus_dir}/output/ \
         --genomeDir ${indices_dir}/HIV.genomeDir/ \
         --readFilesIn ${fastqs}/Unmapped.out.mate1.fastq \
@@ -135,52 +120,50 @@ for i in ${!fastq_links[@]}; do
     fi
 
 
-#    ######================================ Creates BAM index for visual in IGV ================================######
-#    cd ${virus_dir}/output/
-#    samtools index -b -@ 64 Aligned.sortedByCoord.out.bam Aligned.sortedByCoord.out.bam.bai
-
-
     #######================================ Maps to Mega-Virus index ================================#######
     fastqs=${human_dir}/output
-    cd ${fastqs}/
+#    cd ${fastqs}/
 
     if test -f "Unmapped.out.mate2.fastq"; then
         STAR \
-        --runThreadN 64 \
+        --runThreadN 32 \
         --outFileNamePrefix ${mega_virus_dir}/output/ \
         --genomeDir ${indices_dir}/new_virus.genomeDir/ \
         --readFilesIn ${fastqs}/Unmapped.out.mate1.fastq ${fastqs}/Unmapped.out.mate2.fastq \
         --outFilterMultimapNmax 1 \
-        --outSAMtype BAM SortedByCoordinate
+        --outSAMtype SAM
     else
         STAR \
-        --runThreadN 64 \
+        --runThreadN 32 \
         --outFileNamePrefix ${mega_virus_dir}/output/ \
         --genomeDir ${indices_dir}/new_virus.genomeDir/ \
         --readFilesIn ${fastqs}/Unmapped.out.mate1.fastq \
         --outFilterMultimapNmax 1 \
-        --outSAMtype BAM SortedByCoordinate
+        --outSAMtype SAM
     fi
 
     for file in *.fastq; do
         rm -f $file
     done
+    
+    unset filenames
+done
 
 
-#    ######================================ Creates mega-index virus species output ================================######
-#    cd ${mega_virus_dir}/output/
-#    samtools view -h -o Aligned.sortedByCoord.sam Aligned.sortedByCoord.out.bam
-#
-#    grep -v @ Aligned.sortedByCoord.sam |cut -f 3|sort|uniq > uniq_aligned.txt
-#    total=$(grep -v @ Aligned.sortedByCoord.sam|wc -l)
-#    while read line
-#    do
-#        counts=$(grep -v @ Aligned.sortedByCoord.sam | grep ${line} |wc -l)
-#        species=$(grep ${line} ${metatable}|cut -f 2)
-#        echo -e "$line\t$species\t$counts\t$total\t"$(bc -l <<< "(($counts / $total)*100)")"%" >> species.counts.txt
-#    done<uniq_aligned.txt
-#
-#    sort -t $'\t' -k 3,3 -r -V species.counts.txt > ordered_species.counts.txt
+######================================ Creates mega-index virus species output ================================######
+cd ${mega_virus_dir}/output/
+samtools view -h -o Aligned.sortedByCoord.sam Aligned.sortedByCoord.out.bam
+
+grep -v @ Aligned.sortedByCoord.sam |cut -f 3|sort|uniq > uniq_aligned.txt
+total=$(grep -v @ Aligned.sortedByCoord.sam|wc -l)
+while read line
+do
+    counts=$(grep -v @ Aligned.sortedByCoord.sam | grep ${line} |wc -l)
+    species=$(grep ${line} ${metatable}|cut -f 2)
+    echo -e "$line\t$species\t$counts\t$total\t"$(bc -l <<< "(($counts / $total)*100)")"%" >> species.counts.txt
+done<uniq_aligned.txt
+
+sort -t $'\t' -k 3,3 -r -V species.counts.txt > ordered_species.counts.txt
 
 
 
@@ -191,7 +174,7 @@ for i in ${!fastq_links[@]}; do
 #    cd ${fastqs}/
 #    if [[ $numfiles == 1 ]]; then
 #        STAR \
-#        --runThreadN 64 \
+#        --runThreadN 32 \
 #        --outFileNamePrefix ${hybrid_dir}/output/ \
 #        --genomeDir ${indices_dir}/hg38HIV.genomeDir/ \
 #        --readFilesIn ${fastqs}/${filenames[0]} \
@@ -201,7 +184,7 @@ for i in ${!fastq_links[@]}; do
 #        --chimOutType Junctions
 #    else
 #        STAR \
-#        --runThreadN 64 \
+#        --runThreadN 32 \
 #        --outFileNamePrefix ${hybrid_dir}/output/ \
 #        --genomeDir ${indices_dir}/hg38HIV.genomeDir/ \
 #        --readFilesIn ${fastqs}/${filenames[0]} ${fastqs}/${filenames[1]} \
@@ -223,7 +206,7 @@ for i in ${!fastq_links[@]}; do
 #    cd ${fastqs}/
 #    if [[ $numfiles == 1 ]]; then
 #        STAR \
-#        --runThreadN 64 \
+#        --runThreadN 32 \
 #        --outFileNamePrefix ${hybrid_sam_dir}/output/ \
 #        --genomeDir ${indices_dir}/hg38HIV.genomeDir/ \
 #        --readFilesIn ${fastqs}/${filenames[0]} \
@@ -233,7 +216,7 @@ for i in ${!fastq_links[@]}; do
 #        --chimOutType SeparateSAMold
 #    else
 #        STAR \
-#        --runThreadN 64 \
+#        --runThreadN 32 \
 #        --outFileNamePrefix ${hybrid_sam_dir}/output/ \
 #        --genomeDir ${indices_dir}/hg38HIV.genomeDir/ \
 #        --readFilesIn ${fastqs}/${filenames[0]} ${fastqs}/${filenames[1]} \
@@ -255,12 +238,9 @@ for i in ${!fastq_links[@]}; do
 #    if [[ $filesize > 0 ]]; then
 #        samtools view -bt /srv/disk00/cheyul1/Venus/STAR/indices/hg38HIV.genomeDir/hg38HIV.fna.fai -u Chimeric.filtered.sam | \
 #        samtools sort -o Chimeric.filtered.sortedByCoord.bam
-#        samtools index -b -@ 64 Chimeric.filtered.sortedByCoord.bam Chimeric.filtered.sortedByCoord.bam.bai
+#        samtools index -b -@ 32 Chimeric.filtered.sortedByCoord.bam Chimeric.filtered.sortedByCoord.bam.bai
 #    else
 #        echo "No Chimeric Transcripts Found"
 #    fi
-    
-    unset filenames
-done
 
 
