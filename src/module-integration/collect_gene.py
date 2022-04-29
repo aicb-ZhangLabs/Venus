4075bc1ebc0e7944f2396ecd5408921d792bd49a
Cheyu <Cheyujlee@gmail.com>
2022-04-29 08:50:48 -0700

    04-29 edited

--- src/module-integration/collect_gene.py
+++ src/module-integration/collect_gene.py
@@ -2,6 +2,22 @@ import pandas as pd
 
 
 def main():
+    def sortChr(value):
+        """ Sort by chromosome """
+        if isinstance(value, int):
+            new = value
+        else:
+            new = value[3:]
+            if new == 'X':
+                new = 23
+            elif new == 'Y':
+                new = 24
+            elif new == 'M':
+                new = 25
+            else:
+                new = int(new)
+        return new
+
     with open("../../../data/guide-Aligned.out.sam", "r") as file:
         quality_reads = []
         for line in file:
@@ -52,14 +68,20 @@ def main():
         elif candidate.shape[0] == 1:
             genes.append(candidate["gene"].to_string(index=False))
         else:
-            # print(candidate["gene"].to_string(index=False))
             gene_concat = ""
             for index2, gene in candidate["gene"].items():
                 gene_concat = gene_concat + gene + " "
-            print(gene_concat)
             genes.append(gene_concat[:-1])
-    print(integration.shape)
-    print(len(genes))
+    integration["gene"] = genes
+    # integration['chr'] = integration['chr'].str.replace(r'\schr', '', regex=True)
+    integration.chr.replace(to_replace="chr", value="", regex=True, inplace=True)
+    integration.chr.replace(to_replace="X", value="23", regex=True, inplace=True)
+    integration.chr.replace(to_replace="Y", value="24", regex=True, inplace=True)
+    integration["chr"] = integration["chr"].astype(int)
+    print(integration)
+    integration.sort_values(by=['chr', 'coord'], ascending=True, inplace=True)
+    class1 = integration[integration["gene"] != ""]
+    print(class1)
 
 
 # Press the green button in the gutter to run the script.
