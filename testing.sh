3e3e937c5de34c2e0861077afe0297b0d0e0d61f
Cheyu <Cheyujlee@gmail.com>
2022-05-10 19:13:17 -0700

    05-10 update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,97 +1,106 @@
 #!/bin/bash
-#SBATCH --job-name=testing_detect
+#SBATCH --job-name=testing1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=24:00:00
 #SBATCH --partition=zhanglab.p
-#SBATCH --output=/srv/disk00/cheyul1/Venus/logs/22-04-24/testing_detect.log
+#SBATCH --output=/srv/disk00/cheyul1/Venus/logs/22-05-10/testing1.log
 
-STAR_dir=/srv/disk00/cheyul1/Venus/STAR
-Venus_dir=/srv/disk00/cheyul1/Venus/repo
+repo_dir=/srv/disk00/cheyul1/Venus/outputs/22-05-10/Venus
+out_dir=/srv/disk00/cheyul1/Venus/outputs/22-05-10/testing1
 
-indices_dir=${STAR_dir}/indices
-data_dir=/srv/disk00/cheyul1/Venus/datasets/test_files
-out_dir=/srv/disk00/cheyul1/Venus/outputs/22-04-24/testing_detect
+
+## Testing Indexing Module
+python3 ${repo_dir}/src/module-index/module-index.py \
+    --humanFASTA ${repo_dir}/reference_files/GCF_000001405.39_GRCh38.p13_genomic.fna \
+    --humanGTF ${repo_dir}/reference_files/GCF_000001405.39_GRCh38.p13_genomic.gtf \
+    --virusFASTA ${repo_dir}/reference_files/mega-virus.fasta \
+    --module detection \
+    --thread 32 \
+    --out ${out_dir}/indices \
+    --hGenome ${out_dir}/indices/human.genomeDir \
+    --virusGenome ${out_dir}/indices/mega_virus.genomeDir
+
+python3 ${repo_dir}/src/module-index/module-index.py \
+    --humanFASTA ${repo_dir}/reference_files/GCF_000001405.39_GRCh38.p13_genomic.fna \
+    --humanGTF ${repo_dir}/reference_files/GCF_000001405.39_GRCh38.p13_genomic.gtf \
+    --virusFASTA ${repo_dir}/reference_files/NC_001802.fna \
+    --virusGTF ${repo_dir}/reference_files/NC_001802.gtf \
+    --module detection \
+    --thread 32 \
+    --out ${out_dir}/indices \
+    --hGenome ${out_dir}/indices/human2.genomeDir \
+    --virusGenome ${out_dir}/indices/HIV2.genomeDir
+    
+python3 ${repo_dir}/src/module-index/module-index.py \
+    --humanFASTA ${repo_dir}/reference_files/GCF_000001405.39_GRCh38.p13_genomic.fna \
+    --humanGTF ${repo_dir}/reference_files/GCF_000001405.39_GRCh38.p13_genomic.gtf \
+    --virusFASTA ${repo_dir}/reference_files/NC_001802.fna \
+    --virusGTF ${repo_dir}/reference_files/NC_001802.gtf \
+    --module integration \
+    --thread 32 \
+    --out ${out_dir}/indices \
+    --hGenome ${out_dir}/indices/hybrid.genomeDir \
+    --virusGenome ${out_dir}/indices/HIV.genomeDir
+
+
 
 # Testing Detection Module
-python3 ${Venus_dir}/module-detection.py \
-    --read ${data_dir}/bulk_1.fastq.gz ${data_dir}/bulk_2.fastq.gz \
+python3 ${repo_dir}/src/module-detection/module-detection.py \
+    --read ${repo_dir}/test_data/bulk_1.fastq.gz \
     --virusThreshold 5 \
-    --virusChrRef ${Venus_dir}/new_virus.species.txt \
-    --virusGenome ${indices_dir}/virus.genomeDir \
-    --humanGenome ${indices_dir}/human.genomeDir \
-    --out ${out_dir}/bulk \
+    --virusChrRef ${repo_dir}/reference_files/virus_chr-ref.tsv \
+    --virusGenome ${out_dir}/indices/mega_virus.genomeDir \
+    --humanGenome ${out_dir}/indices/human.genomeDir \
     --readFilesCommand zcat \
-    --thread 32
+    --thread 32 \
+    --out ${out_dir}/detection/bulk_single-end
     
-python3 ${Venus_dir}/module-detection.py \
-    --read ${data_dir}/singlecell_1cDNA.fastq.gz ${data_dir}/singlecell_2CB+UMI.fastq.gz \
+python3 ${repo_dir}/src/module-detection/module-detection.py \
+    --read ${repo_dir}/test_data/bulk_1.fastq.gz ${repo_dir}/test_data/bulk_2.fastq.gz \
     --virusThreshold 5 \
-    --virusChrRef ${Venus_dir}/new_virus.species.txt \
-    --virusGenome ${indices_dir}/virus.genomeDir \
-    --humanGenome ${indices_dir}/human.genomeDir \
-    --out ${out_dir}/single_cell \
+    --virusChrRef ${repo_dir}/reference_files/virus_chr-ref.tsv \
+    --virusGenome ${out_dir}/indices/mega_virus.genomeDir \
+    --humanGenome ${out_dir}/indices/human.genomeDir \
+    --readFilesCommand zcat \
+    --thread 32 \
+    --out ${out_dir}/detection/bulk_paired-end
+    
+python3 ${repo_dir}/src/module-detection/module-detection.py \
+    --read ${repo_dir}/test_data/singlecell_1cDNA.fastq.gz ${repo_dir}/test_data/singlecell_2CB+UMI.fastq.gz \
+    --virusThreshold 5 \
+    --virusChrRef ${repo_dir}/reference_files/virus_chr-ref.tsv \
+    --virusGenome ${out_dir}/indices/mega_virus.genomeDir \
+    --humanGenome ${out_dir}/indices/human.genomeDir \
     --readFilesCommand zcat \
     --thread 32 \
     --singleCellBarcode 1 16 \
     --singleUniqueMolIdent 17 10 \
-    --singleWhitelist ${indices_dir}/3M-february-2018.txt
+    --singleWhitelist ${repo_dir}/reference_files/3M-february-2018.txt \
+    --out ${out_dir}/detection/single-cell
+
+
 
 # Testing Integration Module
-python3 ${Venus_dir}/module-integration.py \
-    --read ${data_dir}/singlecell_1cDNA.fastq.gz \
-    --virusGenome ${indices_dir}/HIV.genomeDir \
-    --hybridGenome ${indices_dir}/hg38HIV.genomeDir \
-    --guideFASTA ${indices_dir}/integrSeq.genomeDir/integrSeq.fna \
+python3 ${repo_dir}/src/module-integration/module-integration.py \
+    --read ${repo_dir}/test_data/bulk_1.fastq.gz \
+    --virusGenome ${out_dir}/indices/HIV.genomeDir \
+    --hybridGenome ${out_dir}/indices/hybrid.genomeDir \
+    --guideFASTA ${repo_dir}/reference_files/integrSeq.fna \
     --readFilesCommand zcat \
-    --out ${out_dir} \
     --virusChr NC_001802.1 \
     --thread 32 \
-    --geneBed /srv/disk00/cheyul1/genes.bed
+    --geneBed ${repo_dir}/reference_files/genes.bed \
+    --out ${out_dir}/integration/single-end
 
-
-## Testing Indexing Module
-python3 ${Venus_dir}/module-index.py \
-    --hGenome ${out_dir}/hybrid.genomeDir \
-    --humanFASTA ${indices_dir}/human.genomeDir/GCF_000001405.39_GRCh38.p13_genomic.fna \
-    --humanGTF ${indices_dir}/human.genomeDir/GCF_000001405.39_GRCh38.p13_genomic.gtf \
-    --virusGenome ${out_dir}/HIV.genomeDir \
-    --virusFASTA ${indices_dir}/HIV.genomeDir/NC_001802.fna \
-    --virusGTF ${indices_dir}/HIV.genomeDir/NC_001802.gtf \
-    --module integration \
-    --out ${out_dir} \
-    --thread 32
-
-### Printing Test
-#python3 module-index.py \
-#    --out out_dir \
-#    --humanFASTA human.fa \
-#    --humanGTF human.gff \
-#    --virusFASTA virus.fa \
-#    --virusGTF virus.gff \
-#    --humanGenome humanGenome_dir \
-#    --virusGenome virusGenome_dir
-#python3 module-detection.py \
-#    --read read1.fq read2.fq \
-#    --virusGenome virusGenome \
-#    --virusChrRef virus_chr-ref.tsv \
-#    --humanGenome humanGenome \
-#    --out ../data
-#python3 module-detection.py \
-#    --read read1.fq read2.fq \
-#    --virusThreshold 5 \
-#    --virusGenome virusGenome \
-#    --virusChrRef virus_chr-ref.tsv \
-#    --humanGenome humanGenome \
-#    --out ../data \
-#    --singleCellBarcode 1 16 \
-#    --singleUniqueMolIdent 17 10 \
-#    --singleWhitelist whitelist.txt
-#python3 module-integration.py \
-#    --read read1.fq \
-#    --virusGenome virusGenome \
-#    --hybridGenome hybridGenome \
-#    --guideFASTA guide.fa \
-#    --out ../data \
-#    --virusChr NC_001802.1
+python3 ${repo_dir}/src/module-integration/module-integration.py \
+    --read ${repo_dir}/test_data/bulk_1.fastq.gz ${repo_dir}/test_data/bulk_2.fastq.gz \
+    --virusGenome ${out_dir}/indices/HIV.genomeDir \
+    --hybridGenome ${out_dir}/indices/hybrid.genomeDir \
+    --guideFASTA ${repo_dir}/reference_files/integrSeq.fna \
+    --readFilesCommand zcat \
+    --virusChr NC_001802.1 \
+    --thread 32 \
+    --geneBed ${repo_dir}/reference_files/genes.bed \
+    --out ${out_dir}/integration/paired-end
